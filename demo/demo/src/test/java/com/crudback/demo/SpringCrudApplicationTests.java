bf87f64ae3c411fc838540a9dae2732a4150b326
Burhan Zahid <muhammadburhanzahid@gmail.com>
2021-03-09 22:42:45 +0500

    spring initialize

---
+++ demo/demo/src/test/java/com/crudback/demo/SpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudback.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
